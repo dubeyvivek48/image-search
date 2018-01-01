96537acd609aeacaa9821c984044ddd13db7c49f
dubeyvivek48 <dubeyvivek48@gmail.com>
2018-01-01 19:15:23 +0530

    update

--- app/image-list/image-list.component.ts
+++ app/image-list/image-list.component.ts
@@ -1,36 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+
 import {ImageService} from '../Shared/image.service';
 
 
+
 @Component({
+
   selector: 'app-image-list',
+
   templateUrl: './image-list.component.html',
-  styleUrls: ['./image-list.component.css']
+ 
+ styleUrls: ['./image-list.component.css']
+
 })
+
 export class ImageListComponent implements OnInit {
-  images:any[];
+  
+images:any[];
+
   imagesFound:boolean=false;
-  searching:boolean=false;
-  handleSuccess(data){
-    this.imagesFound=true;
-    this.images=data.hits;
-    console.log(data.hits);
-  }
+ 
+ searching:boolean=false;
+ 
+ handleSuccess(data){
+  
+ 	 this.imagesFound=true;
+   
+	 this.images=data.hits;
+   
+	 console.log(data.hits);
+ 
+   }
 
-  handleError(data){
-    console.log(data);
-  }
-  constructor(private _imageService:ImageService) { }
+ 
+ handleError(data){
+ 
+   console.log(data);
 
-  ngOnInit() {
   }
-  searchImages(query:string){
+  
+constructor(private _imageService:ImageService) { }
+
+ 
+ ngOnInit() {
+  }
+ 
+ searchImages(query:string){
+
       this.searching=true;
-    return this._imageService.getImage(query).subscribe(
-      data=>this.handleSuccess(data),
-      error=>this.handleError(error),
-      ()=>this.searching=false,
+   
+      return this._imageService.getImage(query).subscribe(
+  
+     data=>this.handleSuccess(data),
+   
+     error=>this.handleError(error),
+ 
+     ()=>this.searching=false,
     );
-  }
+ 
+ }
 
 }
